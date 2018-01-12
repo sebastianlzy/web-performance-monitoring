bb7f0e1c4e5b6126273c7cd4860aab0c042b4b91
sebastian <sebastian@shopback.com>
2018-01-12 17:45:51 +0800

    monitor shopback ph

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ catch (e){
 }
 
 async function getPerformance() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   const perf = {}
   
